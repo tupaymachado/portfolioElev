c23f444620a7ea67cef1b9816675fde95661975c
Tupay Machado <tupay.machado@gmail.com>
2023-09-11 22:50:42 -0300

    commit inicial, primeiros arrumes no código

---
+++ src/assets/firebaseConfig.jsx
@@ -0,0 +1,16 @@
+import { getFirestore, collection, addDoc, writeBatch, doc, runTransaction, getDoc, updateDoc, setDoc } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRjWsAAB5gaQgf5MgHvGj5yZ7hg_bSJ7c",
+  authDomain: "portfolioelevato.firebaseapp.com",
+  projectId: "portfolioelevato",
+  storageBucket: "portfolioelevato.appspot.com",
+  messagingSenderId: "716144958454",
+  appId: "1:716144958454:web:57bf0401b1cd41625005d7"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db, collection, addDoc, writeBatch, doc, runTransaction, getDoc, updateDoc, setDoc };
