5050ba7ee3c13f209434d4e2d95b0d700cc99267
Tupay Machado <tupay.machado@gmail.com>
2023-09-16 13:30:53 -0300

    ajuste searchbar

--- src/assets/SearchBar.jsx
+++ src/assets/SearchBar.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
 import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore'; // assumindo que você já configurou o firebase
 
+const handleSearch = async (searchTerm) => {
+  const portfolioQuery = query(collection(db, 'portfolio'), where('codigo', '==', searchTerm));
+  const querySnapshot = await getDocs(portfolioQuery);
+  if (querySnapshot.empty) {
+    console.log('Nenhum documento correspondente.');
+    return;
+  }
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, '=>', doc.data());
+  });
+
+}
+
 export function SearchBar() {
   const [searchTerm, setSearchTerm] = useState('');
   const db = getFirestore();
-
-  const handleSearch = async (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-
-    const portfolioQuery = query(collection(db, 'portfolio'), where('codigo', '==', searchTerm));
-    const querySnapshot = await getDocs(portfolioQuery);
-
-    if (querySnapshot.empty) {
-      console.log('Nenhum documento correspondente.');
-      return;
-    }  
-
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, '=>', doc.data());
-    });
-  };
+    // Chame a nova função handleSearch
+    await handleSearch(searchTerm);
+  }
 
   return (
     <form onSubmit={handleSearch}>
